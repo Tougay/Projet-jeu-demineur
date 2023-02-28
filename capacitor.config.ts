1cb4b613c51ceed23eb3fbf5b8336b09cf4f0712
Your Name <you@example.com>
2023-02-28 04:37:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'korioo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
